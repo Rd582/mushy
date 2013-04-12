b9b6a0b525a884a619c8bca0c9af4e591b95aa1c
Rishi Desai <rishidesai1@gmail.com>
2013-04-12 12:07:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mushy::Application.config.secret_token = '6b182f7714bdfde49171f94acc56acba5a01cd3775151121e665080f20dd166fa84b2a80966b70cbed9c289d15df21275994538fbdbdcbc965f25308b2bf5b03'
